e58f32e7232d04b097314c67636a94ed422bd87e
Mark Lubin <melubin@gmail.com>
2012-04-28 16:15:35 -0400

    added axes drawer to project added properties origin and scale

--- Calculator/GraphView.m
+++ Calculator/GraphView.m
@@ -7,37 +7,44 @@
 //
 
 #import "GraphView.h"
+#import "AxesDrawer.h"
+
+@interface GraphView()
+@property CGPoint origin;
+@property double scale;
+@end
 
 @implementation GraphView
 
 @synthesize dataSource = _dataSource;
+@synthesize origin = _origin;
+@synthesize scale = _scale;
 
 
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
+
 
 -(void)pan:(UIPanGestureRecognizer *)recognizer{
     //move origin with pan, maybe share code with tap:
+    [self setNeedsDisplay];
 
 }
 
 -(void)tap:(UITapGestureRecognizer *)recognizer{
     //move origin for triple tap
+    [self setNeedsDisplay];
 }
 
 -(void)pinch:(UIPinchGestureRecognizer *)recognizer{
     //scale when pinched
+    [self setNeedsDisplay];
 }
 
 - (void)drawRect:(CGRect)rect
 {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGRect bounds = self.bounds;
+  /*  [AxesDrawer drawAxesInRect:<#(CGRect)#> originAtPoint:<#(CGPoint)#> scale:<#(CGFloat)#>]; */
     double x = 0;
     double y = [self.dataSource functionValueAtPoint:x];
 }
